2af651c8c7b8ef17cb4556284647c0d7b70e779c
Jinendra Jain <jinendrajain6810@gmail.com>
2022-08-17 11:03:44 +0530

    Form Problem Solved , Dir Layout Updated

--- app.js
+++ app.js
@@ -1,85 +1,47 @@
-var express = require("express");
+const express = require("express");
 require("dotenv").config();
-var helmet =require('helmet');
+const helmet =require('helmet');
 const path = require("path");
+const { connectDatabase } = require("./config/database.config");
 
-var app = express();
-app.use(helmet());
-app.set('views', path.join(__dirname, 'views'));
-app.set('public', path.join(__dirname, 'public'));
+
+const app = express();
+
+// app.use(helmet({ crossOriginEmbedderPolicy: false, originAgentCluster: true }));
+
+// constants
+const PORT = Number(process.env.PORT)|| 5000;
+
+
+// app.use(helmet());
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('public', path.join(__dirname, 'public'));
 //setting view engine to ejs
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-const Links = require("./req/schema.js");
-const database = require("./req/config");
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 
 // Landing page
 app.get("/", async (req, res) => {
   res.status(200).render("index");
 });
-
 // All Events page
+const Links = require("./models/events.schema");
 app.get("/events", async (req, res) => {
   const data = await Links.find().exec();
-  res.render("events",{eventData:data});
+  res.render("events", { eventData: data });
 });
+// ALl Event Routes
+app.use('/event', require('./routes/event.routes'))
 
-// Event Dynamic Page
-app.get("/event/:route", async (req, res) => {
-  const data = await Links.findOne(req.params).exec();
-  if (!data) {
-    res.status(404).send("404 event not foundNot Found");
-  } else {
-    res.render('event',{data})
-  }
-});
 
-app.get("/event/:route/app", async (req, res) => {
-  const data = await Links.findOne(req.params).exec();
-  if (!data) {
-    res.status(404).send("404 event Not Found");
-  } else {
-    if (data.isRegistrationOpen) {
-      if (req.headers["user-agent"].includes("Android")) {
-        res.redirect(
-          "https://play.google.com/store/apps/details?id=tech.developerdhairya.ieee_chitkara"
-        );
-      } else {
-        var Name = "Unknown OS";
-        if (req.headers["user-agent"].includes("Win")) Name = 
-          "Windows OS";
-        if (req.headers["user-agent"].includes("Mac")) Name = 
-          "Macintosh";
-        if (req.headers["user-agent"].includes("Linux")) Name = 
-          "Linux OS";
-        if (req.headers["user-agent"].includes("Android")) Name = 
-          "Android OS";
-        if (req.headers["user-agent"].includes("like Mac")) Name = 
-          "iOS";
-        res.status(200).render('eventStatus',{Status: true,OsName:Name,evntdata:data});
-      }
-    } else {
-      res.status(200).render('eventStatus',{Status:false,data});
-    }
-  }
-});
+connectDatabase().then(()=>{
+  app.listen(PORT, () => { console.log("Listening on port: " + PORT); });
 
-// Event Form Dynamic Page
-app.get("/event/:route/form", async (req, res) => {
-  data = await Links.findOne(req.params).exec();
-  if (!data) {
-    res.status(404).send("404 event not foundNot Found");
-  } else {
-    if (data.isRegistrationOpen) {
-      res.status(200).render('form',{data});
-    } else {
-      res.status(200).render('eventStatus',{Status:false});
-    }
-  }
-});
-
-app.listen(Number(process.env.PORT), () => {
-  console.log("Listening on port: " + process.env.PORT);
-});
+}).catch((err)=>{console.log("Error at Connecting Database ",err)})

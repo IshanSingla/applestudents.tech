3b4f668a19034ea66852088647f558cdb93d014f
Ishan <is.ishan.singla@gmail.com>
2022-08-20 00:31:04 +0530

    small try with sockit io in web app

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const express = require("express");
+const { Server } = require("socket.io");
 require("dotenv").config();
 const path = require("path");
 const { connectDatabase } = require("./config/database.config");
 
 const app = express();
+const server = require("http").createServer(app);
+const io = new Server(server);
 
-// constants
-const PORT = Number(process.env.PORT)|| 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.set('views', path.join(__dirname, 'views'));
-app.set('public', path.join(__dirname, 'public'));
-
-
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("views", path.join(__dirname, "views"));
+app.set("public", path.join(__dirname, "public"));
 
 // ALl Event Routes
-app.use('/', require('./routes/event.routes'))
+app.use("/", require("./routes/event.routes"));
 
-connectDatabase().then(()=>{
-  app.listen(PORT, () => { console.log("Listening on port: " + PORT); });
+require("./models/events.schema").watch().on("change", (data) => {
+  io.emit("chat", { message: "ishan", userName: "ishasingla" });
+});
 
-}).catch((err)=>{console.log("Error at Connecting Database ",err)})
+connectDatabase()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log("Listening on port: " + PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("Error at Connecting Database ", err);
+  });

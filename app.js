fff0b51d4a81f938b38e7f9e0d8ad2a0da9d1697
Ishan <is.ishan.singla@gmail.com>
2022-08-30 04:20:02 +0530

    work done
    removal of sockit which done in experiment
    some update in home page

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const { Server } = require("socket.io");
-const http = require("http");
 require("dotenv").config();
 const path = require("path");
 const { connectDatabase } = require("./config/database.config");
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
 // webapp setups
 app
@@ -19,18 +15,11 @@ app
   .set("public", path.join(__dirname, "public"))
   .use("/", require("./routes/event.routes")); // ALL Events page
 
-// database update call sockit
-require("./models/events.schema")
-  .watch()
-  .on("change", (data) => {
-    io.emit("chat", { message: "ishan", userName: "ishasingla" });
-  });
+// // heroku awake way
+// setInterval(() => {
+//   http.get("http://ieeewebapp.herokuapp.com");
+// }, 300000); // every 5 minutes (300000)
 
-// heroku awake way
-setInterval(() => {
-  http.get("http://ieeewebapp.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
-
-server.listen(Number(process.env.PORT) || 5000, async () => {
+app.listen(Number(process.env.PORT) || 3000, async () => {
   await connectDatabase();
 });

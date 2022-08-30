2a13a8f1977f52a83b63518a362cca98a8e0ef39
Ishan <is.ishan.singla@gmail.com>
2022-08-30 13:59:30 +0530

    done mostly

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -3,80 +3,77 @@ const mongoose = require("mongoose");
 const Links = require("../models/events.schema");
 
 // All Events page
-router.get("/", async(req, res) => {
-  const data = await Links.find().exec();
-  res.render("index",{data});
+router.get("/", async (req, res) => {
+  const data = await Links.find(
+    {
+      $or:
+        [
+          {eventCategory:"current"},
+          {eventCategory:"upcomming"}
+        ]
+    }
+    ).exec();
+  res.render("index", { data });
 });
 
 router.post("/create", async (req, res) => {
-  const { route, eventDescription, eventName, eventPosterURL, eventRegistrationURL, isRegistrationOpen, eventCategory, eventCreationTimestamp } = req.body;
+  const {route, eventDescription, eventName, eventPosterURL, eventRegistrationURL, isRegistrationOpen, eventCategory, eventCreationTimestamp } = req.body;
   new Links({
     _id: new mongoose.Types.ObjectId(),
-    route, eventDescription, eventName, eventPosterURL, eventRegistrationURL, isRegistrationOpen, eventCategory, eventCreationTimestamp
+    route,
+    eventDescription,
+    eventName,
+    eventPosterURL,
+    eventRegistrationURL,
+    isRegistrationOpen,
+    eventCategory,
+    eventCreationTimestamp,
   }).save();
-  res.send("events save sucessfully")
+  res.send("events save sucessfully");
 });
 
 // Event Dynamic Page /event/:route
 router.get("/:route", async (req, res) => {
-
   const data = await Links.findOne(req.params).exec();
-  if (!data) {
-    res.status(404).render("404Error");
-  } else {
+  if (data) {
     res.render("event", { data });
   }
-
 });
 
-// /event/:route/app
-router.get("/:route/app", async (req, res) => {
+// /:route/Register
+router.get("/:route/Register", async (req, res) => {
   const data = await Links.findOne(req.params).exec();
-  if (!data) {
-    res.status(404).render("404Error");
-  } else {
+  if (data) {
     if (data.isRegistrationOpen) {
       if (req.headers["user-agent"].includes("Android")) {
         res.redirect(
           "https://play.google.com/store/apps/details?id=tech.developerdhairya.ieee_chitkara"
         );
       } else {
-        var Name = "Unknown OS";
-        if (req.headers["user-agent"].includes("Win")) Name = "Windows OS";
-        if (req.headers["user-agent"].includes("Mac")) Name = "Macintosh";
-        if (req.headers["user-agent"].includes("Linux")) Name = "Linux OS";
-        if (req.headers["user-agent"].includes("Android")) Name = "Android OS";
-        if (req.headers["user-agent"].includes("like Mac")) Name = "iOS";
-        res
-          .status(200)
-          .render("eventStatus", {
-            Status: true,
-            OsName: Name,
-            evntdata: data,
-          });
+        res.status(200).render("form", { data });
       }
-    } else {
-      res.status(200).render("eventStatus", { Status: false, data });
-    }
-  }
-});
-
-// /event/:route/form
-// Event Form Dynamic Page
-router.get("/:route/form", async (req, res) => {
-  data = await Links.findOne(req.params).exec();
-  if (!data) {
-    res.status(404).render("404Error");
-  } else {
-    if (data.isRegistrationOpen) {
-      res.status(200).render("form", { data });
     } else {
       res.status(200).render("eventStatus", { Status: false });
     }
   }
 });
 
-router.get('*', async (req, res) =>{
+// // /event/:route/form
+// // Event Form Dynamic Page
+// router.get("/:route/form", async (req, res) => {
+//   data = await Links.findOne(req.params).exec();
+//   if (!data) {
+//     res.status(404).render("404Error");
+//   } else {
+//     if (data.isRegistrationOpen) {
+//       res.status(200).render("form", { data });
+//     } else {
+//       res.status(200).render("eventStatus", { Status: false });
+//     }
+//   }
+// });
+
+router.get("*", async (req, res) => {
   res.status(404).render("404Error");
 });
 

40443f2457028df253db6b4b3f8a8c8321344263
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-09-26 22:45:50 +0530

    Testing particle js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 function App() {
+  const particlesInit = async (main) => {
+    console.log(main);
+    await loadFull(main);
+  };
+
   return (
-    <div className='text-5xl'>App</div>
-  )
+    <div className="App">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          fullScreen: {
+            enable: true,
+            zIndex: 0,
+          },
+          particles: {
+            number: {
+              value: 150,
+            },
+            color: {
+              value: "#fff",
+            },
+            shape: {
+              type: "circle",
+            },
+            opacity: {
+              value: 0.8,
+              random: true,
+              anim: {
+                enable: false,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false,
+              },
+            },
+            size: {
+              value: 5,
+              random: false,
+            },
+            move: {
+              enable: true,
+              speed: 1,
+              direction: "bottom",
+              random: false,
+              straight: false,
+              out_mode: "out",
+            },
+          },
+          retina_detect: true,
+          background: {
+            color: "#111",
+            image: "",
+            position: "50% 50%",
+            repeat: "no-repeat",
+            size: "cover",
+          },
+        }}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
